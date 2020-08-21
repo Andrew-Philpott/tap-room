d68027592ecb53e5a7068c57c12d16932de5f7e0
Andrew Philpott <andrewphilpott92@gmail.com>
2020-08-21 10:37:13 -0700

    Update validators.

--- api/Validators/User/UpdateUserValidator.cs
+++ api/Validators/User/UpdateUserValidator.cs
@@ -7,9 +7,9 @@ namespace TapRoomApi.Validators
   {
     public UpdateUserValidator()
     {
-      RuleFor(x => x.FirstName).NotEmpty().Matches("^[a-zA-Z]*$").Length(50);
-      RuleFor(x => x.LastName).NotEmpty().Matches("^[a-zA-Z]*$").Length(50);
-      RuleFor(x => x.UserName).NotEmpty().Matches("^[a-zA-Z0-9]*$").Length(50);
+      RuleFor(x => x.FirstName).NotEmpty().Matches("^[a-zA-Z]*$").MaximumLength(50);
+      RuleFor(x => x.LastName).NotEmpty().Matches("^[a-zA-Z]*$").MaximumLength(50);
+      RuleFor(x => x.UserName).NotEmpty().Matches("^[a-zA-Z0-9]*$").MaximumLength(50);
       RuleFor(x => x.Email).EmailAddress();
       RuleFor(x => x.Password).NotEmpty().Matches("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{10,18}$");
     }
