612e9b6379a856715a4dfd881063f2804f52d9e5
Andrew Philpott <andrewphilpott92@gmail.com>
2020-08-21 09:17:03 -0700

    Create updatereview and createreview validators.

---
+++ api/Validators/reviews/CreateReviewValidator.cs
@@ -0,0 +1,15 @@
+using FluentValidation;
+using TapRoomApi.DataTransferObjects;
+
+namespace TapRoomApi.Validators
+{
+  public class CreateReviewValidator : AbstractValidator<CreateReview>
+  {
+    public CreateReviewValidator()
+    {
+      RuleFor(x => x.Rating).InclusiveBetween(1, 5);
+      RuleFor(x => x.Description).NotEmpty().Matches("^[a-zA-Z0-9 '\".,]*$").Length(50, 500);
+      RuleFor(x => x.BeerId).NotEmpty();
+    }
+  }
+}
